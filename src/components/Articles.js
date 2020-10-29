3e97f90027fb933bea5ab8b61bf912429e9df3fd
Marcos Leite <30819867+mabner@users.noreply.github.com>
2020-10-29 01:55:39 -0300

    Adds an mage to the articles

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -4,10 +4,25 @@ export const Articles = (props) => (
 	<div className="main-body">
 		{props.items.map((item) => (
 			<div className="article-body">
-				<p className="article-title" id={item.link}>
-					{item.title}
-				</p>
-				<p className="article-text">{item.text}</p>
+				<div className="article-items">
+					<img src="articles.jpg" alt="News Paper" />
+					<span className="imageText">
+						Photo by{' '}
+						<a href="https://unsplash.com/@anniespratt?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+							Annie Spratt
+						</a>{' '}
+						on{' '}
+						<a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+							Unsplash
+						</a>
+					</span>
+				</div>
+				<div className="article-items">
+					<p className="article-title" id={item.link}>
+						{item.title}
+					</p>
+					<p className="article-text">{item.text}</p>
+				</div>
 			</div>
 		))}
 	</div>
